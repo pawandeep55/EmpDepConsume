bf2cb917046667a5144053d55c0fb1235e589660
Pawan Deep Singh <pawan.deep55@gmail.com>
2018-02-15 17:28:04 +0530

    delete added and update modal added and insert working

--- EmpDepConsume/consumejs.js
+++ EmpDepConsume/consumejs.js
@@ -1,9 +1,9 @@
 ﻿$(document).ready(function () {
-  
-   var table= $('#myTable').DataTable({
+
+    var table = $('#myTable').DataTable({
         "ajax": {
             url: "http://localhost:57671/api/emp/get",
-            dataSrc: "" 
+            dataSrc: ""
         }
         , "columns": [
             //{ "data": "EmpId" },
@@ -12,22 +12,46 @@
             {
                 "data": "EmpId",
                 render: function (data) {
-                    return "<button class='btn btn-danger js-delete' data-empid="+data+">Delete</button>"
-                        }
+                    return "<button class='btn btn-info js-edit' data-empid=" + data + " data-toggle='modal' data-target='#myModal'>Edit</button>" + "  " + "<button class='btn btn-danger js-delete' data-empid=" + data + ">Delete</button>"
+                }
             }
-        ]   
             //,
-            //"columnDefs": [{
-            //    "defaultContent": "-",
-            //    "targets": "_all"
-            //}]
+            //{
+            //    "data": "EmpId",
+            //    render: function (data) {
+            //        return "<button class='btn btn-danger js-delete' data-empid=" + data + ">Delete</button>"
+            //    }
+            //}
+            
+        ]
+        //,
+        //"columnDefs": [{
+        //    "defaultContent": "-",
+        //    "targets": "_all"
+        //}]
 
     })
 
-   $('#myTable').on('click', '.js-delete', function () {
-       var button = $(this);
-       alert(button.attr('data-empid'))
-   })
+    $('#myTable').on('click', '.js-delete', function () {
+        var button = $(this);
+        bootbox.confirm("Are you sure you want to delete this Employee?", function (result) {
+            if (result) {
+                $.ajax({
+                    url: "http://localhost:57671/api/emp/" + button.attr("data-empid"),
+                    method: "DELETE",
+                    success: function () {
+                        table.row(button.parents("tr")).remove().draw();
+                    }
+                })
+            }
+        })
+    })
+
+    //$('#jsinsert').click(function () {
+    //    alert('yoo')
+
+    //})
+
 
     $('#clickme').click(function () {
         $.ajax({
@@ -39,7 +63,7 @@
                 var obj = JSON.stringify(json)
                 console.log(json)
                 $.each(json, function (key, value) {
-                    var el = $('<tr>')  
+                    var el = $('<tr>')
 
                     $.each(value, function (key, value) {
                         console.log('key: ' + key + ' value:' + value)
